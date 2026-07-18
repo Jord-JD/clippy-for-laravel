4ea5918ce5f909ce05e1bf20b02b79e35857fbfb
Jordan Hall <jordan@hall05.co.uk>
2026-07-18 11:04:20 +0100

    Secure Clippy output and support current Laravel

--- src/Clippy.php
+++ src/Clippy.php
@@ -4,6 +4,8 @@ namespace JordJD\ClippyForLaravel;
 
 class Clippy
 {
+    const CLIPPY_VERSION = '8bfd1f92c725c6bc3a435d538a77c3302b327c08';
+
     private $expression;
 
     public function __construct(string $expression)
@@ -14,19 +16,35 @@ class Clippy
     public function render()
     {
         return '
-                <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/smore-inc/clippy.js@master/build/clippy.css" media="all">
-                <script src="https://code.jquery.com/jquery-1.7.min.js"></script>
-                <script src="https://cdn.jsdelivr.net/gh/smore-inc/clippy.js@master/build/clippy.min.js"></script>
+                <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/clippyjs/clippy.js@'.self::CLIPPY_VERSION.'/build/clippy.css" media="all">
+                <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/gh/clippyjs/clippy.js@'.self::CLIPPY_VERSION.'/build/clippy.min.js"></script>
                 <script>
                     $(document).ready(function() {
                         clippy.load(\'Clippy\', function(agent){
                             agent.show();
                             agent.animate();
-                            agent.speak(\'<?php echo with('.$this->expression.') ?>\');
+                            agent.speak(<?php echo \\JordJD\\ClippyForLaravel\\Clippy::encodeSpeech(with('.$this->expression.')); ?>);
                             agent.animate();
                         });
                     });
                 </script>
             ';
+    }
+
+    /**
+     * Encode speech as a JavaScript string without allowing HTML or script injection.
+     *
+     * @param mixed $speech
+     * @return string
+     */
+    public static function encodeSpeech($speech)
+    {
+        $encoded = json_encode(
+            (string) $speech,
+            JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT
+        );
+
+        return $encoded === false ? '""' : $encoded;
     }
 }
